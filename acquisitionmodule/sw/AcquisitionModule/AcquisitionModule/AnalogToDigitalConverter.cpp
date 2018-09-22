390cbc1aada4be1abc324bb5024880d59544fb5e
Robert Gawron <robert_gawron@yahoo.fr>
2018-09-22 16:04:04 +0200

    empty classes for various stuff

---
+++ acquisitionmodule/sw/AcquisitionModule/AcquisitionModule/AnalogToDigitalConverter.cpp
@@ -0,0 +1,19 @@
+/* 
+* AnalogToDigitalConverter.cpp
+*
+* Created: 22.09.2018 16:03:39
+* Author: robert
+*/
+
+
+#include "AnalogToDigitalConverter.h"
+
+// default constructor
+AnalogToDigitalConverter::AnalogToDigitalConverter()
+{
+} //AnalogToDigitalConverter
+
+// default destructor
+AnalogToDigitalConverter::~AnalogToDigitalConverter()
+{
+} //~AnalogToDigitalConverter
---
+++ acquisitionmodule/sw/AcquisitionModule/AcquisitionModule/Display.cpp
@@ -0,0 +1,19 @@
+/* 
+* Display.cpp
+*
+* Created: 22.09.2018 16:02:47
+* Author: robert
+*/
+
+
+#include "Display.h"
+
+// default constructor
+Display::Display()
+{
+} //Display
+
+// default destructor
+Display::~Display()
+{
+} //~Display
