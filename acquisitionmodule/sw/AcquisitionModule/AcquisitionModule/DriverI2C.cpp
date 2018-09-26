bbd429a326c6c80e65e8a314b5602a9a9e368fa8
Robert Gawron <robert_gawron@yahoo.fr>
2018-09-26 11:31:07 +0200

    adapter for i2c lib

--- acquisitionmodule/sw/AcquisitionModule/AcquisitionModule/DriverI2C.cpp
+++ acquisitionmodule/sw/AcquisitionModule/AcquisitionModule/DriverI2C.cpp
@@ -7,22 +7,40 @@
 
 
 #include "DriverI2C.h"
-#include "TinyWireM/TinyWireM.h"
 
-DriverI2C::DriverI2C()
-{
-}
-
-
-DriverI2C::~DriverI2C()
-{
-}
+USI_TWI DriverI2C::tinyWire;
 
 bool DriverI2C::init()
 {
-    TinyWireM.begin();
+    tinyWire.begin();
 
-    // no way to check if it pass or failed, but for future
-    //I'm adding returning status of initialization in all drivers
+    // There is no way to check if initialization was successfully,
+    // but to keep consistency with other drivers this method returns
+    // status, for now it's always true, might be useful in future.
     return true;
+}
+
+void DriverI2C::beginTransmission(uint8_t slaveAddr)
+{
+    tinyWire.beginTransmission(slaveAddr);
+}
+
+void DriverI2C::send(uint8_t data)
+{
+    tinyWire.send(data);
+}
+
+uint8_t DriverI2C::endTransmission()
+{
+    return tinyWire.endTransmission();
+}
+
+uint8_t DriverI2C::requestFrom(uint8_t slaveAddr, uint8_t numBytes)
+{
+    return tinyWire.requestFrom(slaveAddr, numBytes);
+}
+
+uint8_t DriverI2C::receive()
+{
+    return tinyWire.receive();
 }
