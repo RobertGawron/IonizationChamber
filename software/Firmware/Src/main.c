af519c66311e21341d12f3169f51fb9ce3bbfa0c
Robert Gawron <robert_gawron@yahoo.fr>
2019-06-04 13:41:17 +0200

    initial sw version

--- software/Firmware/Src/main.c
+++ software/Firmware/Src/main.c
@@ -2,7 +2,34 @@
  * main.c
  *
  *  Created on: 04.06.2019
- *      Author: robert
+ *  Author: robert. Based on stm8s-sdcc-template.
  */
 
+#include "stm8s.h"
+#include "stm8s_it.h"
+#include "ApplicationBuilder.h"
 
+int main( void )
+{
+	ApplicationBuilder_Init();
+
+	while(TRUE)
+	{
+		ApplicationBuilder_Tick();
+	}
+}
+
+#ifdef USE_FULL_ASSERT
+
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+	(void)file;
+	(void)line;
+
+	while (TRUE)
+	{
+		// empty
+	}
+}
+
+#endif
