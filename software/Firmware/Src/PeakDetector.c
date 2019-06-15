384b722ed66af75784a8ab7d8b12adb6be9169ca
Robert Gawron <robert_gawron@yahoo.fr>
2019-06-15 16:33:57 +0200

    draft of firmware

---
+++ software/Firmware/Src/PeakDetector.c
@@ -0,0 +1,18 @@
+/*
+ * PeakDetector.c
+ *
+ *  Created on: 15 cze 2019
+ *      Author: robert
+ */
+
+#include "PeakDetector.h"
+
+bool PeakDetector_Init(uint8_t pin)
+{
+
+}
+
+void PeakDetector_Tick()
+{
+
+}
