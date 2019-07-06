788ef085b59038544457d1dda32f743cacecdd3a
RobertGawron <robert_gawron@yahoo.fr>
2019-07-06 16:07:17 +0100

    cleanup in sensors part

--- software/Firmware/Src/ApplicationBuilder.c
+++ software/Firmware/Src/ApplicationBuilder.c
@@ -12,7 +12,8 @@
 #include "Logger.h"
 #include "PulseCounter.h"
 #include "UserInterface.h"
-#include "MCP3425A0T.h"
+#include "VoltageSensorActualValue".h
+#include "VoltageSensorPeakValue.h"
 
 static MCP3425A0TConfig_t adcConfig[]={{PIN_ADC_CHIP_1}, {PIN_ADC_CHIP_2}};
 
