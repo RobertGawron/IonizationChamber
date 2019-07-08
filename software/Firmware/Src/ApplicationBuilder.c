b3246e4644b47af83d4ba9342f51c7410685f9bb
RobertGawron <robert_gawron@yahoo.fr>
2019-07-08 17:48:13 +0100

    cleanup

--- software/Firmware/Src/ApplicationBuilder.c
+++ software/Firmware/Src/ApplicationBuilder.c
@@ -20,23 +20,16 @@ static MCP3425A0TConfig_t adcConfig[]={{PIN_ADC_CHIP_1}, {PIN_ADC_CHIP_2}};
 
 void ApplicationBuilder_Init()
 {
-	/*uint8_t amountOfADC = sizeof(adcConfig) / sizeof(adcConfig[0]);
+    ClockConfigurator_Init();
+    TimerConfigurator_Init();
 
-	for(uint8_t i = 0U; i < amountOfADC; i++)
-	{
-		MCP3425A0T_Init(&adcConfig[i]);
-	}*/
-
-	ClockConfigurator_Init();
-	TimerConfigurator_Init();
-
-	Logger_Init();
-	PulseCounter_Init(PIN_PULSE_COUNTER);
+    Logger_Init();
+    PulseCounter_Init(PIN_PULSE_COUNTER);
     UserInterface_Init();
 
     enableInterrupts();
 
-    UserInterface_ShowMessage(GUI_FAULT_MSG);
+    UserInterface_ShowMessage(USER_INTERFAE_STATE_OK_MSG);
 }
 
 void ApplicationBuilder_Run()
--- software/Firmware/Src/UserInterface.c
+++ software/Firmware/Src/UserInterface.c
@@ -1,5 +1,5 @@
 /*
- * GUI.c
+ * UserInterface.c
  *
  *  Created on: 30.06.2019
  *      Author: robert
@@ -9,22 +9,24 @@
 
 #include "PinoutConfiguration.h"
 
+#define GPIO_LED_PINS (PIN_GPIO_LED_GREEN | PIN_GPIO_LED_RED)
+
 void UserInterface_Init()
 {
-    GPIO_Init(PORT_GPIO_LED, (PIN_GPIO_LED_GREEN | PIN_GPIO_LED_RED),  GPIO_MODE_OUT_PP_LOW_SLOW);
-    GPIO_WriteLow(PORT_GPIO_LED, (PIN_GPIO_LED_GREEN | PIN_GPIO_LED_RED));
+    GPIO_Init(PORT_GPIO_LED, GPIO_LED_PINS,  GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_WriteLow(PORT_GPIO_LED, GPIO_LED_PINS);
 }
 
-void UserInterface_ShowMessage(GUI_Status status)
+void UserInterface_ShowMessage(UserInterface_Status status)
 {
     switch(status)
     {
-        case GUI_FAULT_MSG:
+        case USER_INTERFACE_COLLECTING_DATA_MSG:
         {
             GPIO_WriteHigh(PORT_GPIO_LED, PIN_GPIO_LED_RED);
             break;
         }
-        case GUI_INFO_MSG:
+        case USER_INTERFAE_STATE_OK_MSG:
         {
             GPIO_WriteHigh(PORT_GPIO_LED, PIN_GPIO_LED_GREEN);
             break;
