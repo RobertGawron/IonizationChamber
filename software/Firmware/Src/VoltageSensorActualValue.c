d86c1d9da8257c773169ab4ef8218c0e0d68d285
RobertGawron <robert_gawron@yahoo.fr>
2019-07-28 11:50:51 +0100

    sending dummy data

--- software/Firmware/Src/VoltageSensorActualValue.c
+++ software/Firmware/Src/VoltageSensorActualValue.c
@@ -58,18 +58,18 @@ bool VoltageSensorActualValue_GeMeasurementData(VoltageSensorActualValue_Measure
     I2C_GenerateSTART(ENABLE);
     while(!I2C_CheckEvent(I2C_EVENT_MASTER_MODE_SELECT));
 
-//    I2C_Send7bitAddress((I2C_SLAVE_ADDRESS << 1), I2C_DIRECTION_TX);
-//    while(!I2C_CheckEvent(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+    I2C_Send7bitAddress((I2C_SLAVE_ADDRESS << 1), I2C_DIRECTION_TX);
+    while(!I2C_CheckEvent(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
 
-    I2C_Send7bitAddress((I2C_SLAVE_ADDRESS << 1), I2C_DIRECTION_RX);
-    while(!I2C_CheckEvent(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+//    I2C_Send7bitAddress((I2C_SLAVE_ADDRESS << 1), I2C_DIRECTION_RX);
+//    while(!I2C_CheckEvent(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
 
 //    UserInterface_ShowMessage(USER_INTERFACE_COLLECTING_DATA_MSG);
 
 
     I2C_SendData(0xf);
-//I2C_ReceiveData();
-//    while(!I2C_CheckEvent(I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+    //I2C_ReceiveData();
+        while(!I2C_CheckEvent(I2C_EVENT_MASTER_BYTE_TRANSMITTED));
 
 
 
